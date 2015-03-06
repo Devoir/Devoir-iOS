399c8b8dd3a1cbb78dd8b50ed2dafce5d1379a0a
Brent Roberts <brentjoeroberts@gmail.com>
2015-03-06 00:41:05 -0700

    style changes

--- devoir-ios/CourseTableViewCell.m
+++ devoir-ios/CourseTableViewCell.m
@@ -13,6 +13,7 @@
 
 - (void)setupCellWithWidth:(int)width Height:(int)height ForRow:(int)row {
     self.courseLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, 70)];
+    self.courseLabel.textAlignment = NSTextAlignmentCenter;
     if(row == 0)
     {
         self.courseLabel.text = @"Show All Courses";
@@ -25,7 +26,7 @@
         self.courseScrollView.contentSize = CGSizeMake(600, 70);
         self.courseScrollView.showsHorizontalScrollIndicator = NO;
         self.courseScrollView.showsVerticalScrollIndicator = NO;
-        self.courseScrollView.bounces = NO;
+        self.courseScrollView.bounces = YES;
         [self.courseScrollView addSubview:self.courseLabel];
         
         //tansparent button to cover the filter by course section
@@ -42,14 +43,14 @@
         self.editButton = [UIButton buttonWithType:UIButtonTypeContactAdd];
         self.editButton.tag = row - 1;
         self.editButton.frame = CGRectMake(0, 0, 65, 70);
-        self.editButton.backgroundColor = [UIColor devPurple];
+        //self.editButton.backgroundColor = [UIColor devPurple];
         [self.courseDeleteEditView addSubview:self.editButton];
         
         //button for edit course button
         self.deleteButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
         self.deleteButton.tag = row -1;
         self.deleteButton.frame = CGRectMake(65, 0, 65, 70);
-        self.deleteButton.backgroundColor = [UIColor devBlue];
+        //self.deleteButton.backgroundColor = [UIColor devBlue];
         [self.courseDeleteEditView addSubview:self.deleteButton];
         
         [self.courseScrollView addSubview:self.courseDeleteEditView];
